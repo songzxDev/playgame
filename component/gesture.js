107ee6a408abdbff9bcb0d13dd52df060ceecc0e
songzxDev <songzx_2326@163.com>
2019-08-18 16:57:53 +0800

    调整了部分js变量位置

--- component/gesture.js
+++ component/gesture.js
@@ -6,14 +6,14 @@ function enableGesture(main){
 
         context.isTap = true;
         context.isPan = false;
-    }
+    };
     let move = (point, context) => {
         //console.log(context.startX,context.startY);
         let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
         if(dx * dx + dy * dy > 100) {
             context.isTap = false;
 
-            if(context.isPan == false) {
+            if(!context.isPan) {
                 let e = new Event("panstart");
                 e.startX = context.startX;
                 e.startY = context.startY;
@@ -28,27 +28,27 @@ function enableGesture(main){
             main.dispatchEvent(e);
         }
         //console.log(dx , dy);
-    }
+    };
     let end = (point, context) => {
         //console.log("end")
+        let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
         if(context.isTap) {
             let e = new Event("tap");
             main.dispatchEvent(e);
         }
         if(context.isPan) {
             let e = new Event("panend");
-            let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
             e.dx = dx;
             e.dy = dy;
             main.dispatchEvent(e);
         }
-    }
+    };
     let cancel = (point, context) => {
         if(context.isPan) {
             let e = new Event("pancancel");
             main.dispatchEvent(e);
         }
-    }
+    };
     let contexts = Object.create(null);
 
     let mouseSymbol = Symbol("mouse");
@@ -58,17 +58,17 @@ function enableGesture(main){
         document.addEventListener("mouseup",mouseup);
         contexts[mouseSymbol] = Object.create(null);
         start(event, contexts[mouseSymbol]);
-    }
+    };
     let mousemove = event => {
         move(event, contexts[mouseSymbol]);
-    }
+    };
 
     let mouseup = event => {
         document.removeEventListener("mousemove", mousemove);
         document.removeEventListener("mouseup",mouseup);
         end(event, contexts[mouseSymbol]);
         delete contexts[mouseSymbol];
-    }
+    };
     main.addEventListener("mousedown", mousedown);
 
 
@@ -79,25 +79,28 @@ function enableGesture(main){
             start(touch, contexts[touch.identifier]);
         }
 
-    }
+    };
     let touchmove = event => {
         for(let touch of event.changedTouches)
             move(touch, contexts[touch.identifier]);
-    }
+    };
     let touchend = event => {
         for(let touch of event.changedTouches) {
             end(touch, contexts[touch.identifier]);
             delete contexts[touch.identifier];
         }
-    }
+    };
     let touchcancel = event => {
         for(let touch of event.changedTouches){
             cancel(touch, contexts[touch.identifier]);
             delete contexts[touch.identifier];
         }
-    }
-    main.addEventListener("touchstart", touchstart);
-    main.addEventListener("touchmove", touchmove);
-    main.addEventListener("touchend", touchend);
-    main.addEventListener("touchcancel", touchcancel);
+    };
+    window.onload = function() {
+        main.addEventListener("touchstart", touchstart);
+        main.addEventListener("touchmove", touchmove);
+        main.addEventListener("touchend", touchend);
+        main.addEventListener("touchcancel", touchcancel);
+    };
+
 }
