7b965bc9b39fb3238cc1793c031a31d58f7deb88
songzxDev <songzx_2326@163.com>
2019-10-12 09:58:55 +0800

    17.电话号码的字母组合（回朔算法）

--- leatcode/lib/test04.js
+++ leatcode/lib/test04.js
@@ -507,3 +507,34 @@ const toGoatLatin = function (S) {
     }
     return ss.join(' ');
 };
+
+/**
+ * 17.电话号码的字母组合（回朔算法）
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = function (digits) {
+    let phone = new Map([['2', 'abc'], ['3', 'def'], ['4', 'ghi'], ['5', 'jkl'], ['6', 'mno'], ['7', 'pqrs'], ['8', 'tuv'], ['9', 'wxyz']]);
+    let backtrack = (comb, next) => {
+        if (comb.length === 0) {
+            comb = next.split('');
+        } else {
+            let tmp = [];
+            for (let curr of comb) {
+                for (let s of next.split('')) {
+                    tmp.push(curr + s);
+                }
+            }
+            comb = tmp;
+        }
+        return comb;
+    };
+    let res = [];
+    for (let dg of digits) {
+        if (phone.has(dg)) {
+            res = backtrack(res, phone.get(dg));
+        }
+    }
+    return res;
+};
+
