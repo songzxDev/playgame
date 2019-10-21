9db043fe5d915620558aa04c41c5c25214cafcd2
songzxDev <songzx_2326@163.com>
2019-10-21 09:02:38 +0800

    599.两个列表的最小索引总和（哈希表）

--- leatcode/lib/test05.js
+++ leatcode/lib/test05.js
@@ -359,3 +359,25 @@ const longestPalindrome = function (s) {
     }
     return ans;
 };
+
+/**
+ * 599.两个列表的最小索引总和（哈希表）
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+const findRestaurant = function (list1, list2) {
+    let [set1, set2] = [new Set(list1), new Set(list2)];
+    let [res, tmpIdx] = [[], -1];
+    set1.forEach(function (value) {
+        if (set2.has(value)) {
+            let currIdx = list1.indexOf(value) + list2.indexOf(value);
+            if (tmpIdx === -1 || currIdx <= tmpIdx) {
+                res.push(value);
+                tmpIdx = currIdx;
+            }
+
+        }
+    });
+    return res;
+};
