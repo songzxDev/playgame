3d9584504d28ab797099be1b02dc0334141aa6cf
songzxDev <songzx_2326@163.com>
2019-10-23 09:47:22 +0800

    712.两个字符串的最小ASCII删除和【动态规划】

--- leatcode/lib/test05.js
+++ leatcode/lib/test05.js
@@ -432,3 +432,30 @@ const findLHS = function (nums) {
     }
     return ans;
 };
+
+/**
+ * 712.两个字符串的最小ASCII删除和【动态规划】
+ * @param {string} s1
+ * @param {string} s2
+ * @return {number}
+ */
+const minimumDeleteSum = function (s1, s2) {
+    let dp = Array.from({length: s1.length + 1}, () => (Array.from({length: s2.length + 1}, () => (0))));
+    for (let i = s1.length - 1; i >= 0; i--) {
+        dp[i][s2.length] = dp[i + 1][s2.length] + s1.charCodeAt(i);
+    }
+    for (let j = s2.length - 1; j >= 0; j--) {
+        dp[s1.length][j] = dp[s1.length][j + 1] + s2.charCodeAt(j);
+    }
+
+    for (let i = s1.length - 1; i >= 0; i--) {
+        for (let j = s2.length - 1; j >= 0; j--) {
+            if (s1.charAt(i) === s2.charAt(j)) {
+                dp[i][j] = dp[i + 1][j + 1];
+            } else {
+                dp[i][j] = Math.min(dp[i + 1][j] + s1.charCodeAt(i), dp[i][j + 1] + s2.charCodeAt(j));
+            }
+        }
+    }
+    return dp[0][0];
+};
