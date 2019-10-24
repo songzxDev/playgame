3c7a56e7f2858f77ef07e2f1a0a1373d4c342a5a
songzxDev <songzx_2326@163.com>
2019-10-24 08:05:08 +0800

    647.回文子串（动态规划）

--- leatcode/lib/test05.js
+++ leatcode/lib/test05.js
@@ -459,3 +459,37 @@ const minimumDeleteSum = function (s1, s2) {
     }
     return dp[0][0];
 };
+
+/**
+ * 647.回文子串（动态规划）
+ * @param {string} s
+ * @return {number}
+ */
+const countSubstrings = function (s) {
+
+    let buildDPForCountSubstrings = (str) => {
+        let n = str.length;
+        let dp = Array.from({length: n}, () => (Array.from({length: n}, () => (false))));
+        for (let j = 0; j < n; j++) {
+            for (let i = 0; i <= j; i++) {
+                if (i === j) {
+                    dp[i][j] = true;
+                } else {
+                    dp[i][j] = str.charAt(i) === str.charAt(j) && (j - i <= 1 || dp[i + 1][j - 1]);
+                }
+            }
+        }
+        return dp;
+    };
+    if (s === null || s.length === 0) {
+        return 0;
+    }
+    let res = 0;
+    let dp = buildDPForCountSubstrings(s);
+    for (let j = 0; j < dp.length; j++) {
+        for (let i = 0; i <= j; i++) {
+            if (dp[i][j]) res += 1;
+        }
+    }
+    return res;
+};
