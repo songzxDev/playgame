cf316ed439aab6ace6edac800bd4ed124f3d660c
songzxDev <songzx_2326@163.com>
2019-10-28 16:19:41 +0800

    64.最小路径和（数组、动态规划）

--- leatcode/lib/test06.js
+++ leatcode/lib/test06.js
@@ -20,3 +20,28 @@ const longestCommonSubsequence = function (text1, text2) {
     }
     return dp[text1.length][text2.length];
 };
+
+/**
+ * 64.最小路径和（数组、动态规划）
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const minPathSum = function (grid) {
+    let dp = Array.from({length: grid.length}, () => (
+        Array.from({length: grid[0].length}, () => (0)))
+    );
+    for (let i = grid.length - 1; i >= 0; i--) {
+        for (let j = grid[0].length - 1; j >= 0; j--) {
+            if (i === grid.length - 1 && j !== grid[0].length - 1) {
+                dp[i][j] = grid[i][j] + dp[i][j + 1];
+            } else if (j === grid[0].length - 1 && i !== grid.length - 1) {
+                dp[i][j] = grid[i][j] + dp[i + 1][j];
+            } else if (j !== grid[0].length - 1 && i !== grid.length - 1) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
+            } else {
+                dp[i][j] = grid[i][j];
+            }
+        }
+    }
+    return dp[0][0];
+};
