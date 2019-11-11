0713fa25fe187ef69dac6f979a983db1ffacda87
songzxDev <songzx_2326@163.com>
2019-11-11 16:51:35 +0800

    /**  * 题号：283.移动零  * 标签：数组、双指针  * @param {number[]} nums  * @return {void} Do not return anything, modify nums in-place instead.  */

--- leatcode/lib/test07.js
+++ leatcode/lib/test07.js
@@ -22,3 +22,26 @@ const merge = function (nums1, m, nums2, n) {
         nums1[i] = nums2[i];
     }
 };
+
+/**
+ * 题号：283.移动零
+ * 标签：数组、双指针
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = function (nums) {
+    if (nums.length === 0) return;
+    let [i, lastNum] = [0, nums.length - 1];
+    while (i < lastNum) {
+        if (nums[i] === 0) {
+            let j = i;
+            while (j < lastNum) {
+                nums[j] = nums[++j];
+            }
+            nums[lastNum--] = 0;
+        } else {
+            i++;
+        }
+    }
+};
+moveZeroes([0, 1, 0, 3, 12])
