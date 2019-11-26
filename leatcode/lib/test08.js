f8c034f5fd1d5a90799a35546a0bbea571c0002b
songzxDev <songzx_2326@163.com>
2019-11-26 11:38:07 +0800

    /**  * 题目：1004.最大连续1的个数 III  * 标签：双指针 滑动窗口  * @param {number[]} A  * @param {number} K  * @return {number}  */

--- leatcode/lib/test08.js
+++ leatcode/lib/test08.js
@@ -219,12 +219,12 @@ const numberOfSubarrays = function (nums, k) {
  * @param {number} k
  * @return {number}
  */
-const numberOfSubarraysFast = function(nums, k) {
+const numberOfSubarraysFast = function (nums, k) {
     const atMost = (arry, k) => {
         let res = 0, i = 0, n = arry.length;
         for (let j = 0; j < n; j++) {
             k -= arry[j] % 2;
-            while(k < 0) {
+            while (k < 0) {
                 k += arry[i++] % 2;
             }
             res += j - i + 1;
@@ -234,4 +234,48 @@ const numberOfSubarraysFast = function(nums, k) {
     return atMost(nums, k) - atMost(nums, k - 1);
 };
 
-console.log(numberOfSubarrays(nums = [2, 2, 2, 1, 2, 2, 1, 2, 2, 2], k = 2));
+// console.log(numberOfSubarrays(nums = [2, 2, 2, 1, 2, 2, 1, 2, 2, 2], k = 2));
+
+/**
+ * 题目：1004.最大连续1的个数 III
+ * 标签：双指针 滑动窗口
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+const longestOnes = function (A, K) {
+    /*给定一个由若干 0 和 1 组成的数组 A，我们最多可以将 K 个值从 0 变成 1 。
+    返回仅包含 1 的最长（连续）子数组的长度。
+    示例 1：
+    输入：A = [1,1,1,0,0,0,1,1,1,1,0], K = 2
+    输出：6
+    解释：
+    [1,1,1,0,0,1,1,1,1,1,1]
+    粗体数字从 0 翻转到 1，最长的子数组长度为 6。
+    示例 2：
+    输入：A = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], K = 3
+    输出：10
+    解释：
+    [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+    粗体数字从 0 翻转到 1，最长的子数组长度为 10。
+    提示：
+    1 <= A.length <= 20000
+    0 <= K <= A.length
+    A[i] 为 0 或 1
+    Related Topics 双指针 Sliding Window*/
+
+    let zeros = 0, start = 0, ans = 0;
+    for (let end = 0; end < A.length; end++) {
+        if (A[end] === 0) zeros++;
+        while (zeros > K) {
+            if (A[start++] === 0) zeros--;
+        }
+        ans = Math.max(ans, end - start + 1);
+    }
+    return ans;
+};
+console.log(longestOnes(A = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], K = 2));
+console.log(longestOnes(A = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], K = 3));
+console.log(longestOnes(A = [0, 0, 1, 1, 1, 0, 0], K = 0));
+console.log(longestOnes(A = [1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1]
+    , K = 8));
