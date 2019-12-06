3a68743056a370b8aa512dbc606bd70dcb05ea87
songzxDev <songzx_2326@163.com>
2019-12-06 15:50:36 +0800

    三数之和

--- leatcode/lib/test09.js
+++ leatcode/lib/test09.js
@@ -104,3 +104,35 @@ const removeDuplicates = function (nums) {
     }
     return j;
 };
+
+/**
+ * 题目：15.三数之和
+ * 标签：数组 双指针
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = function(nums) {
+    let res = [];
+    if (!nums || nums.length < 3) {
+        return res;
+    }
+    nums.sort((a, b) => a - b);
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i === 0 || nums[i] > nums[i - 1]) {
+            let j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                let add = nums[i] + nums[j] + nums[k];
+                if (add > 0) {
+                    k--;
+                } else if (add < 0) {
+                    j++;
+                } else {
+                    res.push([nums[i], nums[j], nums[k]]);
+                    while (j < k && nums[j] === nums[++j]);
+                    while (j < k && nums[k] === nums[--k]);
+                }
+            }
+        }
+    }
+    return res;
+};
