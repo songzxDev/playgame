84775016f4702e297dd84acc519d8e07f5cae444
songzxDev <songzx_2326@163.com>
2019-12-14 09:48:11 +0800

    /**  * 题目：76.最小覆盖子串（https://leetcode-cn.com/problems/minimum-window-substring/）  * 标签：哈希表 双指针 字符串 滑动窗口  * 学号：1034  * @param {string} s  * @param {string} t  * @return {string}  */

---
+++ leatcode/lib/test10.js
@@ -0,0 +1,56 @@
+//给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字母的最小子串。
+//
+// 示例：
+//
+// 输入: S = "ADOBECODEBANC", T = "ABC"
+//输出: "BANC"
+//
+// 说明：
+//
+//
+// 如果 S 中不存这样的子串，则返回空字符串 ""。
+// 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+//
+// Related Topics 哈希表 双指针 字符串 Sliding Window
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：76.最小覆盖子串（https://leetcode-cn.com/problems/minimum-window-substring/）
+ * 标签：哈希表 双指针 字符串 滑动窗口
+ * 学号：1034
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+const minWindow = function (s, t) {
+    if (!s || !t || s.length < t.length) {
+        return '';
+    }
+    const getCountArray = () => [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let mapT = getCountArray(), winds = getCountArray(), i = 0, j = 0, minLen = s.length + 1, found = 0, start = 0;
+    for (let k = 0; k < t.length; k++) {
+        mapT[t.charCodeAt(k)]++;
+    }
+    while (j < s.length) {
+        if (found < t.length) {
+            let right = s.charCodeAt(j++);
+            if (mapT[right] > 0) {
+                if (++winds[right] <= mapT[right]) {
+                    found++;
+                }
+            }
+        }
+        while (found === t.length) {
+            if (j - i < minLen) {
+                [start, minLen] = [i, j - i];
+            }
+            let left = s.charCodeAt(i++);
+            if (mapT[left] > 0) {
+                if (--winds[left] < mapT[left]) {
+                    found--;
+                }
+            }
+        }
+    }
+    return minLen === s.length + 1 ? '' : s.substr(start, minLen);
+};
+//leetcode submit region end(Prohibit modification and deletion)
