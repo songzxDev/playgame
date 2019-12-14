a739c88cc063e1ef2490bb047546f7dfb344c629
songzxDev <songzx_2326@163.com>
2019-12-14 20:30:51 +0800

    三数之和

--- leatcode/lib/test10.js
+++ leatcode/lib/test10.js
@@ -16,7 +16,7 @@
 /**
  * 题目：76.最小覆盖子串（https://leetcode-cn.com/problems/minimum-window-substring/）
  * 标签：哈希表 双指针 字符串 滑动窗口
- * 学号：1034
+ * 学号：1034（五期三班一组）
  * @param {string} s
  * @param {string} t
  * @return {string}
@@ -52,5 +52,51 @@ const minWindow = function (s, t) {
         }
     }
     return minLen === s.length + 1 ? '' : s.substr(start, minLen);
+};
+
+//leetcode submit region end(Prohibit modification and deletion)
+//给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+// 注意：答案中不可以包含重复的三元组。
+// 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+//满足要求的三元组集合为：
+//[
+//  [-1, 0, 1],
+//  [-1, -1, 2]
+//]
+// Related Topics 数组 双指针
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：15.三数之和（https://leetcode-cn.com/problems/3sum/）
+ * 标签：双指针 数组
+ * 学号：1034（五期三班一组）
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = function (nums) {
+    let res = [];
+    nums.sort((a, b) => a - b);
+    if (nums[0] > 0 || nums.length < 3) {
+        return res;
+    }
+    for (let k = 0; k < nums.length - 2; k++) {
+        if (k === 0 || nums[k] > nums[k - 1]) {
+            let i = k + 1, j = nums.length - 1;
+            while (i < j) {
+                let afterAdd = nums[k] + nums[i] + nums[j];
+                if (afterAdd < 0) {
+                    i++;
+                } else if (afterAdd > 0) {
+                    j--;
+                } else {
+                    res.push([nums[k], nums[i], nums[j]]);
+                    while (i < j && nums[i] === nums[++i]) {
+                    }
+                    while (i < j && nums[j] === nums[--j]) {
+                    }
+                }
+            }
+        }
+    }
+    return res;
 };
 //leetcode submit region end(Prohibit modification and deletion)
