436113181db0290c81dab2eedae1ac55594cf22e
songzxDev <songzx_2326@163.com>
2019-12-14 21:05:15 +0800

    /**  * 题目：84.柱状图中的最大矩形面积（https://leetcode-cn.com/problems/largest-rectangle-in-histogram/）  * @param {number[]} heights  * @return {number}  */ const largestRectangleArea = function (heights) {     Array.prototype.peek = function () {         return this.length > 0 ? this[this.length - 1] : null;     };     let stack = [-1], maxArea = 0;     for (let k = 0; k < heights.length; k++) {         while (stack.peek() > -1 && heights[stack.peek()] >= heights[k]) {             maxArea = Math.max(maxArea, heights[stack.pop()] * (k - stack.peek() - 1));         }         stack.push(k);     }     while (stack.peek() > -1) {         maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - stack.peek() - 1));     }     return maxArea; };

--- leatcode/lib/test10.js
+++ leatcode/lib/test10.js
@@ -100,3 +100,39 @@ const threeSum = function (nums) {
     return res;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+// 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+//
+// 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+//
+// 图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+//
+// 示例:
+//
+// 输入: [2,1,5,6,2,3]
+//输出: 10
+// Related Topics 栈 数组
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：84.柱状图中的最大矩形面积（https://leetcode-cn.com/problems/largest-rectangle-in-histogram/）
+ * @param {number[]} heights
+ * @return {number}
+ */
+const largestRectangleArea = function (heights) {
+    Array.prototype.peek = function () {
+        return this.length > 0 ? this[this.length - 1] : null;
+    };
+    let stack = [-1], maxArea = 0;
+    for (let k = 0; k < heights.length; k++) {
+        while (stack.peek() > -1 && heights[stack.peek()] >= heights[k]) {
+            maxArea = Math.max(maxArea, heights[stack.pop()] * (k - stack.peek() - 1));
+        }
+        stack.push(k);
+    }
+    while (stack.peek() > -1) {
+        maxArea = Math.max(maxArea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+    }
+    return maxArea;
+};
+//leetcode submit region end(Prohibit modification and deletion)
