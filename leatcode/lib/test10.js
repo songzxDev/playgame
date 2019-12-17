98fbfea46e654d2ee75e25c66d801b8333740aa1
songzxDev <songzx_2326@163.com>
2019-12-17 13:09:25 +0800

    /**  * 题目：98.验证二叉搜索树（https://leetcode-cn.com/problems/validate-binary-search-tree/）  * 标签：树 递归 深度优先搜索  * 学号：1034（五期一班三组）  * @param {TreeNode} root  * @return {boolean}  */ const isValidBST = function (root) {     let cur = root, pre = null, stack = [];     while (stack.length > 0 || cur) {         if (cur) {             stack.push(cur);             cur = cur.left;         } else {             let p = stack.pop();             if (pre && p.val <= pre.val) {                 return false;             }             pre = p;             cur = p.right;         }     }     return true; };

--- leatcode/lib/test10.js
+++ leatcode/lib/test10.js
@@ -409,3 +409,57 @@ const invertTree = function (root) {
     return root;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+
+//给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+// 假设一个二叉搜索树具有如下特征：
+// 节点的左子树只包含小于当前节点的数。
+// 节点的右子树只包含大于当前节点的数。
+// 所有左子树和右子树自身必须也是二叉搜索树。
+// 示例 1:
+//
+// 输入:
+//    2
+//   / \
+//  1   3
+//输出: true
+//
+// 示例 2:
+//
+// 输入:
+//    5
+//   / \
+//  1   4
+//     / \
+//    3   6
+//输出: false
+//解释: 输入为: [5,1,4,null,null,3,6]。
+//     根节点的值为 5 ，但是其右子节点值为 4 。
+//
+// Related Topics 树 深度优先搜索
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：98.验证二叉搜索树（https://leetcode-cn.com/problems/validate-binary-search-tree/）
+ * 标签：树 递归 深度优先搜索
+ * 学号：1034（五期一班三组）
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isValidBST = function (root) {
+    let cur = root, pre = null, stack = [];
+    while (stack.length > 0 || cur) {
+        if (cur) {
+            stack.push(cur);
+            cur = cur.left;
+        } else {
+            let p = stack.pop();
+            if (pre && p.val <= pre.val) {
+                return false;
+            }
+            pre = p;
+            cur = p.right;
+        }
+    }
+    return true;
+};
+//leetcode submit region end(Prohibit modification and deletion)
