e127c80e2e29d863acef16526e99b46ca76dc1ce
songzxDev <songzx_2326@163.com>
2019-12-17 12:05:30 +0800

    /**  * 题目：226.翻转二叉树（https://leetcode-cn.com/problems/invert-binary-tree/）  * 标签：树 递归  * 学号：1034（五期一班三组）  * @param {TreeNode} root  * @return {TreeNode}  */ const invertTree = function (root) {     const helper = (tree) => {         if (!tree || (!tree.left && !tree.right)) {             return;         }         [tree.left, tree.right] = [tree.right, tree.left];         helper(tree.left);         helper(tree.right);     };     helper(root);     return root; };

--- leatcode/lib/test10.js
+++ leatcode/lib/test10.js
@@ -354,3 +354,58 @@ const climbStairs = function (n) {
     return storage[n];
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//翻转一棵二叉树。
+//
+// 示例：
+//
+// 输入：
+//
+//     4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9
+//
+// 输出：
+//
+//     4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1
+//
+// 备注:
+//这个问题是受到 Max Howell 的 原问题 启发的 ：
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+// Related Topics 树
+//leetcode submit region begin(Prohibit modification and deletion)
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+/**
+ * 题目：226.翻转二叉树（https://leetcode-cn.com/problems/invert-binary-tree/）
+ * 标签：树 递归
+ * 学号：1034（五期一班三组）
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = function (root) {
+    const helper = (tree) => {
+        if (!tree || (!tree.left && !tree.right)) {
+            return;
+        }
+        [tree.left, tree.right] = [tree.right, tree.left];
+        helper(tree.left);
+        helper(tree.right);
+    };
+    helper(root);
+    return root;
+};
+//leetcode submit region end(Prohibit modification and deletion)
