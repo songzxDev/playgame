7cc673619ec6713e9870ad32675e30aa8c862ef1
songzxDev <songzx_2326@163.com>
2019-12-17 09:29:24 +0800

    /**  * 题目：70.爬楼梯（https://leetcode-cn.com/problems/climbing-stairs/）  * 标签：树 递归  * 学号：1034（五期一班三组）  * @param {number} n  * @return {number}  */ const climbStairs = function (n) {     if (n < 4) {         return n;     }     const helper = (a, n, storage) => {         // f(n) = f(n - 1) + f(n - 2);         if (a > n) {             return;         }         storage.push(storage[a - 1] + storage[a - 2]);         helper(a + 1, n, storage);     };     let storage = [0, 1, 2, 3];     helper(4, n, storage);     return storage[n]; };

--- leatcode/lib/test10.js
+++ leatcode/lib/test10.js
@@ -303,3 +303,54 @@ const generateParenthesis = function (n) {
     return res;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+//
+// 注意：给定 n 是一个正整数。
+//
+// 示例 1：
+//
+// 输入： 2
+//输出： 2
+//解释： 有两种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶
+//2.  2 阶
+//
+// 示例 2：
+//
+// 输入： 3
+//输出： 3
+//解释： 有三种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶 + 1 阶
+//2.  1 阶 + 2 阶
+//3.  2 阶 + 1 阶
+//
+// Related Topics 动态规划
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：70.爬楼梯（https://leetcode-cn.com/problems/climbing-stairs/）
+ * 标签：树 递归
+ * 学号：1034（五期一班三组）
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function (n) {
+    if (n < 4) {
+        return n;
+    }
+    const helper = (a, n, storage) => {
+        // f(n) = f(n - 1) + f(n - 2);
+        if (a > n) {
+            return;
+        }
+        storage.push(storage[a - 1] + storage[a - 2]);
+        helper(a + 1, n, storage);
+    };
+    let storage = [0, 1, 2, 3];
+    helper(4, n, storage);
+    return storage[n];
+};
+//leetcode submit region end(Prohibit modification and deletion)
