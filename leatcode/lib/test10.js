16a82d0736829d545ded80b0586c1c17577da012
songzxDev <songzx_2326@163.com>
2019-12-17 14:48:41 +0800

    /**  * 题目：104.二叉树的最大深度（https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/）  * 标签：树 深度优先搜索  * 学号：1034（五期一班三组）  * @param {TreeNode} root  * @return {number}  */ const maxDepth = function (root) {     const helper = (tree) => {         if (!tree) {             return 0;         }         return Math.max(helper(tree.left), helper(tree.right)) + 1;     };     return helper(root); };

--- leatcode/lib/test10.js
+++ leatcode/lib/test10.js
@@ -463,3 +463,39 @@ const isValidBST = function (root) {
     return true;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个二叉树，找出其最大深度。
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例：
+//给定二叉树 [3,9,20,null,null,15,7]，
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+// 返回它的最大深度 3 。
+// Related Topics 树 深度优先搜索
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：104.二叉树的最大深度（https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/）
+ * 标签：树 深度优先搜索
+ * 学号：1034（五期一班三组）
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function (root) {
+    const helper = (tree) => {
+        if (!tree) {
+            return 0;
+        }
+        return Math.max(helper(tree.left), helper(tree.right)) + 1;
+    };
+    return helper(root);
+};
+//leetcode submit region end(Prohibit modification and deletion)
