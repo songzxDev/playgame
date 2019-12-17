bf652c6d2a7cb35694d27b5585287debfde7ef93
songzxDev <songzx_2326@163.com>
2019-12-17 15:27:53 +0800

    /**  * 题目：111.二叉树的最小深度（https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/）  * 标签：树 深度优先搜索  * 学号：1034（五期一班三组）  * @param {TreeNode} root  * @return {number}  */ const minDepth = function (root) {     const helper = (tree) => {         if (!tree) {             return 0;         }         if (!tree.left && !tree.right) {             return 1;         }         let minNum = 2147483647;         if (tree.left) {             minNum = Math.min(helper(tree.left), minNum);         }         if (tree.right) {             minNum = Math.min(helper(tree.right), minNum);         }         return minNum + 1;     };     return helper(root); };

--- leatcode/lib/test10.js
+++ leatcode/lib/test10.js
@@ -499,3 +499,50 @@ const maxDepth = function (root) {
     return helper(root);
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个二叉树，找出其最小深度。
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例:
+//
+// 给定二叉树 [3,9,20,null,null,15,7],
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+// 返回它的最小深度 2.
+// Related Topics 树 深度优先搜索 广度优先搜索
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：111.二叉树的最小深度（https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/）
+ * 标签：树 深度优先搜索
+ * 学号：1034（五期一班三组）
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const minDepth = function (root) {
+    const helper = (tree) => {
+        if (!tree) {
+            return 0;
+        }
+        if (!tree.left && !tree.right) {
+            return 1;
+        }
+        let minNum = 2147483647;
+        if (tree.left) {
+            minNum = Math.min(helper(tree.left), minNum);
+        }
+        if (tree.right) {
+            minNum = Math.min(helper(tree.right), minNum);
+        }
+        return minNum + 1;
+    };
+    return helper(root);
+};
+//leetcode submit region end(Prohibit modification and deletion)
