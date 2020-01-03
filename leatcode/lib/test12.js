0ded4be1a35b528f955973933a2f12fa1fdbc91e
songzxDev <songzx_2326@163.com>
2020-01-03 14:59:05 +0800

    /**  * @param {number} n  * @return {number}  */ const climbStairs = function (n) {     const memo = new Map([[1, 1], [2, 2], [3, 3]]);     const helper = (n) => {         return memo.has(n) ? memo.get(n) : memo.set(n, helper(n - 1) + helper(n - 2)).get(n);     };     return helper(n); };

--- leatcode/lib/test12.js
+++ leatcode/lib/test12.js
@@ -87,7 +87,7 @@ const ladderLength = function (beginWord, endWord, wordList) {
  * @param {number[]} nums
  * @return {number[][]}
  */
-const permute = function(nums) {
+const permute = function (nums) {
     const helper = (perms, perm, nums) => {
         if (perm.length === nums.length) {
             perms.push([...perm]);
@@ -106,3 +106,40 @@ const permute = function(nums) {
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+//
+// 注意：给定 n 是一个正整数。
+//
+// 示例 1：
+//
+// 输入： 2
+//输出： 2
+//解释： 有两种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶
+//2.  2 阶
+//
+// 示例 2：
+//
+// 输入： 3
+//输出： 3
+//解释： 有三种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶 + 1 阶
+//2.  1 阶 + 2 阶
+//3.  2 阶 + 1 阶
+//
+// Related Topics 动态规划
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function (n) {
+    const memo = new Map([[1, 1], [2, 2], [3, 3]]);
+    const helper = (n) => {
+        return memo.has(n) ? memo.get(n) : memo.set(n, helper(n - 1) + helper(n - 2)).get(n);
+    };
+    return helper(n);
+};
+//leetcode submit region end(Prohibit modification and deletion)
