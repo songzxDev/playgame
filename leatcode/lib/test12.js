9b136d15d693a851d8da3162226962fcfe170b46
songzxDev <songzx_2326@163.com>
2020-01-03 17:55:02 +0800

    /**  * @param {string} digits  * @return {string[]}  */ const letterCombinations = function (digits) {     const MEMO = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];     if (digits.length > 0) {         const helper = (res, cur, depth, digits) => {             if (depth === digits.length) {                 res.push(cur);                 return res;             }             let coolie = MEMO[digits.charCodeAt(depth) - '0'.charCodeAt(0)];             for (let k of coolie) {                 helper(res, cur + k, depth + 1, digits);             }             return res;         };         return helper([], '', 0, digits);     }     return []; };

--- leatcode/lib/test12.js
+++ leatcode/lib/test12.js
@@ -143,3 +143,44 @@ const climbStairs = function (n) {
     return helper(n);
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+//
+//
+//
+// 示例:
+//
+// 输入："23"
+//输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+//
+//
+// 说明:
+//尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+// Related Topics 字符串 回溯算法
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = function (digits) {
+    const MEMO = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
+    if (digits.length > 0) {
+        const helper = (res, cur, depth, digits) => {
+            if (depth === digits.length) {
+                res.push(cur);
+                return res;
+            }
+            let coolie = MEMO[digits.charCodeAt(depth) - '0'.charCodeAt(0)];
+            for (let k of coolie) {
+                helper(res, cur + k, depth + 1, digits);
+            }
+            return res;
+        };
+        return helper([], '', 0, digits);
+    }
+    return [];
+};
+//leetcode submit region end(Prohibit modification and deletion)
