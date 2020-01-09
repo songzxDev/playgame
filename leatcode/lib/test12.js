eb70658ff93a1e5fac64df316199898c2e28d784
songzxDev <songzx_2326@163.com>
2020-01-09 19:39:50 +0800

    /**  * 题目：62.不同路径（https://leetcode-cn.com/problems/unique-paths/）  * 学号：1034（五期一班三组）  * @param {number} m  * @param {number} n  * @return {number}  */ const uniquePaths = function (m, n) {     let pathMemo = Object.create(null);     const helper = (m, n) => {         if (m < 0 || n < 0) {             return 0;         } else if (m === 0 || n === 0) {             return 1;         } else {             let mnKey = `${m}_${n}`;             if (!(mnKey in pathMemo)) {                 pathMemo[mnKey] = helper(m - 1, n) + helper(m, n - 1);             }             return pathMemo[mnKey];         }     };     return helper(m - 1, n - 1); };

--- leatcode/lib/test12.js
+++ leatcode/lib/test12.js
@@ -256,3 +256,47 @@ let head = {
 };
 reverseList(head);
 //leetcode submit region end(Prohibit modification and deletion)
+
+//一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+// 问总共有多少条不同的路径？
+// 例如，上图是一个7 x 3 的网格。有多少可能的路径？
+// 说明：m 和 n 的值均不超过 100。
+// 示例 1:
+// 输入: m = 3, n = 2
+//输出: 3
+//解释:
+//从左上角开始，总共有 3 条路径可以到达右下角。
+//1. 向右 -> 向右 -> 向下
+//2. 向右 -> 向下 -> 向右
+//3. 向下 -> 向右 -> 向右
+// 示例 2:
+// 输入: m = 7, n = 3
+//输出: 28
+// Related Topics 数组 动态规划
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：62.不同路径（https://leetcode-cn.com/problems/unique-paths/）
+ * 学号：1034（五期一班三组）
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+const uniquePaths = function (m, n) {
+    let pathMemo = Object.create(null);
+    const helper = (m, n) => {
+        if (m < 0 || n < 0) {
+            return 0;
+        } else if (m === 0 || n === 0) {
+            return 1;
+        } else {
+            let mnKey = `${m}_${n}`;
+            if (!(mnKey in pathMemo)) {
+                pathMemo[mnKey] = helper(m - 1, n) + helper(m, n - 1);
+            }
+            return pathMemo[mnKey];
+        }
+    };
+    return helper(m - 1, n - 1);
+};
+//leetcode submit region end(Prohibit modification and deletion)
