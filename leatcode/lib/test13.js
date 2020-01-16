75897f47e78acfd0a22b1f633a9e6f24d75986cf
songzxDev <songzx_2326@163.com>
2020-01-16 14:04:13 +0800

    题目22 五毒神掌

--- leatcode/lib/test13.js
+++ leatcode/lib/test13.js
@@ -40,3 +40,42 @@ const uniquePaths = function (m, n) {
     return ans;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+//
+// 例如，给出 n = 3，生成结果为：
+//
+// [
+//  "((()))",
+//  "(()())",
+//  "(())()",
+//  "()(())",
+//  "()()()"
+//]
+//
+// Related Topics 字符串 回溯算法
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const generateParenthesis = function (n) {
+    let res = [];
+    const helper = (res, left, right, s, n) => {
+        if (left + right === (n << 1)) {
+            res.push(s);
+            return;
+        }
+        if (left < n) {
+            helper(res, left + 1, right, s + '(', n);
+        }
+        if (right < left) {
+            helper(res, left, right + 1, s + ')', n);
+        }
+    };
+
+    helper(res, 0, 0, '', n);
+    return res;
+};
+console.log(generateParenthesis(3));
+//leetcode submit region end(Prohibit modification and deletion)
