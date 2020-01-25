966ef9c7784968336d80ad7a22ae96c4adf66085
songzxDev <songzx_2326@163.com>
2020-01-25 10:57:39 +0800

    /**  * 题目：46.全排列（https://leetcode-cn.com/problems/permutations/）  * 学号：1034（五期一班三组）  * @param {number[]} nums  * @return {number[][]}  */ const permute = function(nums) {     const helper = (res, tmp, nums) => {         if (tmp.length === nums.length) {             res.push([...tmp]);             return res;         }         for (let num of nums) {             if (!tmp.includes(num)) {                 tmp.push(num);                 helper(res, tmp, nums);                 tmp.pop();             }         }         return res;     };     return helper([], [], nums); }; //leetcode submit region end(Prohibit modification and deletion)

--- leatcode/depot/depot01.js
+++ leatcode/depot/depot01.js
@@ -74,3 +74,45 @@ const generateParenthesis = function(n) {
     return helper(0, 0, [], '', n);
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个没有重复数字的序列，返回其所有可能的全排列。
+//
+// 示例:
+//
+// 输入: [1,2,3]
+//输出:
+//[
+//  [1,2,3],
+//  [1,3,2],
+//  [2,1,3],
+//  [2,3,1],
+//  [3,1,2],
+//  [3,2,1]
+//]
+// Related Topics 回溯算法
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：46.全排列（https://leetcode-cn.com/problems/permutations/）
+ * 学号：1034（五期一班三组）
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = function(nums) {
+    const helper = (res, tmp, nums) => {
+        if (tmp.length === nums.length) {
+            res.push([...tmp]);
+            return res;
+        }
+        for (let num of nums) {
+            if (!tmp.includes(num)) {
+                tmp.push(num);
+                helper(res, tmp, nums);
+                tmp.pop();
+            }
+        }
+        return res;
+    };
+    return helper([], [], nums);
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
