5db3f7c5b34b81d3a7d51a7c59a1da371a80c2f0
songzxDev <songzx_2326@163.com>
2020-01-28 20:25:56 +0800

    //leetcode submit region begin(Prohibit modification and deletion) /**  * 题目：64.最小路径和（https://leetcode-cn.com/problems/minimum-path-sum/）  * 学号：1034（五期一班三组）  * @param {number[][]} grid  * @return {number}  */ const minPathSum = function (grid) {     if (!grid || grid.length < 1) return 0;     let m = grid[0].length, n = grid.length;     for (let k = 0; k < n; k++) {         for (let p = 0; p < m; p++) {             if (k > 0 && p > 0) grid[k][p] += Math.min(grid[k - 1][p], grid[k][p - 1]);             if (k === 0 && p > 0) grid[k][p] += grid[k][p - 1];             if (k > 0 && p === 0) grid[k][p] += grid[k - 1][p];         }     }     return grid[n - 1][m - 1]; }; //leetcode submit region end(Prohibit modification and deletion)

--- leatcode/depot/depot01.js
+++ leatcode/depot/depot01.js
@@ -339,3 +339,40 @@ const longestValidParentheses = (s) => {
     return res;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+//
+// 说明：每次只能向下或者向右移动一步。
+//
+// 示例:
+//
+// 输入:
+//[
+//  [1,3,1],
+//  [1,5,1],
+//  [4,2,1]
+//]
+//输出: 7
+//解释: 因为路径 1→3→1→1→1 的总和最小。
+//
+// Related Topics 数组 动态规划
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：64.最小路径和（https://leetcode-cn.com/problems/minimum-path-sum/）
+ * 学号：1034（五期一班三组）
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const minPathSum = function (grid) {
+    if (!grid || grid.length < 1) return 0;
+    let m = grid[0].length, n = grid.length;
+    for (let k = 0; k < n; k++) {
+        for (let p = 0; p < m; p++) {
+            if (k > 0 && p > 0) grid[k][p] += Math.min(grid[k - 1][p], grid[k][p - 1]);
+            if (k === 0 && p > 0) grid[k][p] += grid[k][p - 1];
+            if (k > 0 && p === 0) grid[k][p] += grid[k - 1][p];
+        }
+    }
+    return grid[n - 1][m - 1];
+};
+//leetcode submit region end(Prohibit modification and deletion)
