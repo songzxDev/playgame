b8e89298e7f20264cc5a7c2add7c7b3bc59594c5
songzxDev <songzx_2326@163.com>
2020-01-28 11:00:33 +0800

    //leetcode submit region begin(Prohibit modification and deletion) /**  * 题目：322.零钱兑换（https://leetcode-cn.com/problems/coin-change/）  * 学号：1034（五期一班三组）  * @param {number[]} coins  * @param {number} amount  * @return {number}  */ const coinChange = function (coins, amount) {     let max = amount + 1, dp = [0];     for (let i = 1; i <= amount; i++) {         dp.push(max);         for (let coin of coins) if (coin <= i) dp[i] = Math.min(dp[i], dp[i - coin] + 1);     }     return dp[amount] > amount ? -1 : dp[amount]; }; //leetcode submit region end(Prohibit modification and deletion)

--- leatcode/depot/depot01.js
+++ leatcode/depot/depot01.js
@@ -229,3 +229,38 @@ const maxProduct = function (nums) {
     return max;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回
+// -1。
+//
+// 示例 1:
+//
+// 输入: coins = [1, 2, 5], amount = 11
+//输出: 3
+//解释: 11 = 5 + 5 + 1
+//
+// 示例 2:
+//
+// 输入: coins = [2], amount = 3
+//输出: -1
+//
+// 说明:
+//你可以认为每种硬币的数量是无限的。
+// Related Topics 动态规划
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：322.零钱兑换（https://leetcode-cn.com/problems/coin-change/）
+ * 学号：1034（五期一班三组）
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+const coinChange = function (coins, amount) {
+    let max = amount + 1, dp = [0];
+    for (let i = 1; i <= amount; i++) {
+        dp.push(max);
+        for (let coin of coins) if (coin <= i) dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    }
+    return dp[amount] > amount ? -1 : dp[amount];
+};
+//leetcode submit region end(Prohibit modification and deletion)
