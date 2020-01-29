022bd89099f7ff789a81c21863d68cc774812ec9
songzxDev <songzx_2326@163.com>
2020-01-29 12:18:05 +0800

    //leetcode submit region begin(Prohibit modification and deletion) /**  * 题目：17.电话号码的字母组合（https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/）  * 学号：1034（五期一班三组）  * @param {string} digits  * @return {string[]}  */ const letterCombinations = function (digits) {     if (!digits) return [];     const TEL = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'],         helper = (res, s, digits, depth) => {             if (depth === digits.length) {                 res.push(s);                 return res;             }             let coolie = TEL[digits.charCodeAt(depth) - '0'.charCodeAt(0)];             for (let c of coolie) helper(res, s + c, digits, depth + 1);             return res;         };     return helper([], '', digits, 0); }; //leetcode submit region end(Prohibit modification and deletion)

--- leatcode/depot/depot01.js
+++ leatcode/depot/depot01.js
@@ -466,10 +466,48 @@ const maximalSquare = function (matrix) {
         for (let j = 1; j <= n; j++) {
             if (matrix[i - 1][j - 1] === '1') {
                 dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j - 1]), dp[i - 1][j]) + 1;
-                result = Math.max(dp[i][j],result);
+                result = Math.max(dp[i][j], result);
             }
         }
     }
     return result ** 2;
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+//
+//
+//
+// 示例:
+//
+// 输入："23"
+//输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+//
+//
+// 说明:
+//尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+// Related Topics 字符串 回溯算法
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：17.电话号码的字母组合（https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/）
+ * 学号：1034（五期一班三组）
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = function (digits) {
+    if (!digits) return [];
+    const TEL = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'],
+        helper = (res, s, digits, depth) => {
+            if (depth === digits.length) {
+                res.push(s);
+                return res;
+            }
+            let coolie = TEL[digits.charCodeAt(depth) - '0'.charCodeAt(0)];
+            for (let c of coolie) helper(res, s + c, digits, depth + 1);
+            return res;
+        };
+    return helper([], '', digits, 0);
 };
 //leetcode submit region end(Prohibit modification and deletion)
