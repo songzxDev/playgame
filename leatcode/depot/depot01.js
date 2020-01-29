fef9b1629e98b6330a2cb3bb7657620193839d60
songzxDev <songzx_2326@163.com>
2020-01-29 11:43:05 +0800

    /**  * 题目：221.最大正方形（https://leetcode-cn.com/problems/maximal-square/solution/zui-da-zheng-fang-xing-by-leetcode/）  * 学号：1034（五期一班三组）  * @param {string[][]} matrix  * @return {number}  */ const maximalSquare = function (matrix) {     if (matrix.length === 0) return 0;     let m = matrix.length, n = matrix[0].length, result = 0;     let dp = [new Array(n + 1).fill(0)];     for (let i = 1; i <= m; i++) {         dp.push(new Array(n + 1).fill(0));         for (let j = 1; j <= n; j++) {             if (matrix[i - 1][j - 1] === '1') {                 dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j - 1]), dp[i - 1][j]) + 1;                 result = Math.max(dp[i][j],result);             }         }     }     return result ** 2; };

--- leatcode/depot/depot01.js
+++ leatcode/depot/depot01.js
@@ -435,3 +435,41 @@ const minDistance = (word1, word2) => {
     return cost[m][n];
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+
+//在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+//
+// 示例:
+//
+// 输入:
+//
+//1 0 1 0 0
+//1 0 1 1 1
+//1 1 1 1 1
+//1 0 0 1 0
+//
+//输出: 4
+// Related Topics 动态规划
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：221.最大正方形（https://leetcode-cn.com/problems/maximal-square/solution/zui-da-zheng-fang-xing-by-leetcode/）
+ * 学号：1034（五期一班三组）
+ * @param {string[][]} matrix
+ * @return {number}
+ */
+const maximalSquare = function (matrix) {
+    if (matrix.length === 0) return 0;
+    let m = matrix.length, n = matrix[0].length, result = 0;
+    let dp = [new Array(n + 1).fill(0)];
+    for (let i = 1; i <= m; i++) {
+        dp.push(new Array(n + 1).fill(0));
+        for (let j = 1; j <= n; j++) {
+            if (matrix[i - 1][j - 1] === '1') {
+                dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j - 1]), dp[i - 1][j]) + 1;
+                result = Math.max(dp[i][j],result);
+            }
+        }
+    }
+    return result ** 2;
+};
+//leetcode submit region end(Prohibit modification and deletion)
