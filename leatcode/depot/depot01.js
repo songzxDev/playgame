eabe4041df610b2026e8e9bb35397eb2b28a8542
songzxDev <songzx_2326@163.com>
2020-01-29 19:33:14 +0800

    //leetcode submit region begin(Prohibit modification and deletion) /**  * 题目：410.分割数组的最大值  * （https://leetcode-cn.com/problems/split-array-largest-sum/solution/fen-ge-shu-zu-de-zui-da-zhi-by-leetcode/）  * 学号：1034（五期一班三组）  * @param {number[]} nums  * @param {number} m  * @return {number}  */ const splitArray = function(nums, m) {     if (Math.max.apply(null, nums) >= 0x7fffffff) return 0x7fffffff;     let valid = (target, nums, m) => {         let count = 1, total = 0;         for (let num of nums) {             total += num;             if (total > target) {                 total = num;                 if (++count > m) return false;             }         }         return true;     }, max = 0, sum = 0;     for (let num of nums) {         max = Math.max(num, max);         sum += num;     }     if (m === 1) return sum;     let left = max, right = sum;     while (left <= right) {         let mid = (left + right) >> 1;         if (valid(mid, nums, m)) {             right = mid - 1;         } else {             left = mid + 1;         }     }     return left; }; //leetcode submit region end(Prohibit modification and deletion)

--- leatcode/depot/depot01.js
+++ leatcode/depot/depot01.js
@@ -579,3 +579,69 @@ const canCross = function (stones) {
     return false;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给定一个非负整数数组和一个整数 m，你需要将这个数组分成 m 个非空的连续子数组。设计一个算法使得这 m 个子数组各自和的最大值最小。
+//
+// 注意:
+//数组长度 n 满足以下条件:
+//
+//
+// 1 ≤ n ≤ 1000
+// 1 ≤ m ≤ min(50, n)
+//
+//
+// 示例:
+//
+//
+//输入:
+//nums = [7,2,5,10,8]
+//m = 2
+//
+//输出:
+//18
+//
+//解释:
+//一共有四种方法将nums分割为2个子数组。
+//其中最好的方式是将其分为[7,2,5] 和 [10,8]，
+//因为此时这两个子数组各自的和的最大值为18，在所有情况中最小。
+//
+// Related Topics 二分查找 动态规划
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：410.分割数组的最大值
+ * （https://leetcode-cn.com/problems/split-array-largest-sum/solution/fen-ge-shu-zu-de-zui-da-zhi-by-leetcode/）
+ * 学号：1034（五期一班三组）
+ * @param {number[]} nums
+ * @param {number} m
+ * @return {number}
+ */
+const splitArray = function(nums, m) {
+    if (Math.max.apply(null, nums) >= 0x7fffffff) return 0x7fffffff;
+    let valid = (target, nums, m) => {
+        let count = 1, total = 0;
+        for (let num of nums) {
+            total += num;
+            if (total > target) {
+                total = num;
+                if (++count > m) return false;
+            }
+        }
+        return true;
+    }, max = 0, sum = 0;
+    for (let num of nums) {
+        max = Math.max(num, max);
+        sum += num;
+    }
+    if (m === 1) return sum;
+    let left = max, right = sum;
+    while (left <= right) {
+        let mid = (left + right) >> 1;
+        if (valid(mid, nums, m)) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return left;
+};
+//leetcode submit region end(Prohibit modification and deletion)
