8570c60c3d0d2cb485e56324e447ec54b71d0915
songzxDev <songzx_2326@163.com>
2020-01-30 12:05:28 +0800

    //leetcode submit region begin(Prohibit modification and deletion) /**  * 题目：51.N皇后（https://leetcode-cn.com/problems/n-queens/）  * 学号：1034（五期一班三组）  * @param {number} n  * @return {string[][]}  */ const solveNQueens = function (n) {     let res = [], cols = new Set(), pie = new Set(), na = new Set(), dfs = (n, row, curState) => {         if (row >= n) {             res.push(curState);             return;         }         for (let c = 0; c < n; c++) {             if (cols.has(c) || pie.has(row + c) || na.has(row - c)) continue;             cols.add(c);             pie.add(row + c);             na.add(row - c);             dfs(n, row + 1, curState.concat([c]));             cols.delete(c);             pie.delete(row + c);             na.delete(row - c);         }     }, afterResult = (n) => {         let after = [], board = res.reduce((all, a) => {             for (let r of a) all.push(''.padEnd(r, '.') + 'Q' + ''.padEnd(n - r - 1, '.'));             return all;         }, []);         for (let i = 0; i < board.length; i += n) after.push(board.slice(i, i + n));         return after;     };     dfs(n, 0, []);     return afterResult(n); }; //leetcode submit region end(Prohibit modification and deletion)

--- leatcode/depot/depot01.js
+++ leatcode/depot/depot01.js
@@ -696,3 +696,66 @@ const countSubstrings = (s) => {
     return count;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+//
+//
+//
+// 上图为 8 皇后问题的一种解法。
+//
+// 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+//
+// 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+//
+// 示例:
+//
+// 输入: 4
+//输出: [
+// [".Q..",  // 解法 1
+//  "...Q",
+//  "Q...",
+//  "..Q."],
+//
+// ["..Q.",  // 解法 2
+//  "Q...",
+//  "...Q",
+//  ".Q.."]
+//]
+//解释: 4 皇后问题存在两个不同的解法。
+//
+// Related Topics 回溯算法
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目：51.N皇后（https://leetcode-cn.com/problems/n-queens/）
+ * 学号：1034（五期一班三组）
+ * @param {number} n
+ * @return {string[][]}
+ */
+const solveNQueens = function (n) {
+    let res = [], cols = new Set(), pie = new Set(), na = new Set(), dfs = (n, row, curState) => {
+        if (row >= n) {
+            res.push(curState);
+            return;
+        }
+        for (let c = 0; c < n; c++) {
+            if (cols.has(c) || pie.has(row + c) || na.has(row - c)) continue;
+            cols.add(c);
+            pie.add(row + c);
+            na.add(row - c);
+            dfs(n, row + 1, curState.concat([c]));
+            cols.delete(c);
+            pie.delete(row + c);
+            na.delete(row - c);
+        }
+    }, afterResult = (n) => {
+        let after = [], board = res.reduce((all, a) => {
+            for (let r of a) all.push(''.padEnd(r, '.') + 'Q' + ''.padEnd(n - r - 1, '.'));
+            return all;
+        }, []);
+        for (let i = 0; i < board.length; i += n) after.push(board.slice(i, i + n));
+        return after;
+    };
+    dfs(n, 0, []);
+    return afterResult(n);
+};
+//leetcode submit region end(Prohibit modification and deletion)
