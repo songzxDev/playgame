e3b9c17835fa79ce0a458fb7cceda793e0e00da4
songzxDev <songzx_2326@163.com>
2020-03-31 20:15:07 +0800

    //leetcode submit region begin(Prohibit modification and deletion) /**  * 题目-912：排序数组 https://leetcode-cn.com/problems/sort-an-array/  * @param {number[]} nums  * @return {number[]}  */ const sortArray = (nums) => {     if (nums.length <= 1) return nums;     let pivot = nums[Math.random() * nums.length | 0];     let lt = nums.filter(n => n < pivot), eq = nums.filter(n => n === pivot), gt = nums.filter(n => n > pivot);     return [...sortArray(lt), ...eq, ...sortArray(gt)]; }; //leetcode submit region end(Prohibit modification and deletion)

--- leatcode/lib/2020/test01.js
+++ leatcode/lib/2020/test01.js
@@ -113,3 +113,47 @@ const uniquePathsWithObstacles = (obstacleGrid) => {
     return steps[0][0];
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+//给你一个整数数组 nums，请你将该数组升序排列。
+//
+//
+//
+//
+//
+//
+// 示例 1：
+//
+// 输入：nums = [5,2,3,1]
+//输出：[1,2,3,5]
+//
+//
+// 示例 2：
+//
+// 输入：nums = [5,1,1,2,0,0]
+//输出：[0,0,1,1,2,5]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 50000
+// -50000 <= nums[i] <= 50000
+//
+//
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * 题目-912：排序数组 https://leetcode-cn.com/problems/sort-an-array/
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortArray = (nums) => {
+    if (nums.length <= 1) return nums;
+    let pivot = nums[Math.random() * nums.length | 0];
+    let lt = nums.filter(n => n < pivot), eq = nums.filter(n => n === pivot), gt = nums.filter(n => n > pivot);
+    return [...sortArray(lt), ...eq, ...sortArray(gt)];
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+console.log(sortArray([99, 89, 0, 1, 9, 7, -1]));
