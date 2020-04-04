1b5d8b4588473e93220647b29aa11de7687990f1
songzxDev <songzx_2326@163.com>
2020-04-04 11:20:18 +0800

    leetcode 15题

--- leatcode/lib/2020/test01.js
+++ leatcode/lib/2020/test01.js
@@ -179,4 +179,50 @@ const sortArray = (nums) => {
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
-console.log(sortArray([999, 0, 9, -98, 76, 35, 2, 1, 4]));
+//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复
+//的三元组。
+//
+// 注意：答案中不可以包含重复的三元组。
+//
+//
+//
+// 示例：
+//
+// 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+//
+//满足要求的三元组集合为：
+//[
+//  [-1, 0, 1],
+//  [-1, -1, 2]
+//]
+//
+// Related Topics 数组 双指针
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const threeSum = (nums) => {
+    if (!nums || nums.length < 3) return [];
+    nums.sort((a, b) => a - b);
+    let res = [], first = nums[0];
+    for (let i = 0; first <= 0 && i < nums.length - 2; i++) {
+        if (i === 0 || nums[i] > nums[i - 1]) {
+            let j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                const add = nums[i] + nums[j] + nums[k];
+                if (add === 0) {
+                    res.push([nums[i], nums[j], nums[k]]);
+                    while (j < k && nums[j] === nums[++j]) {}
+                    while (j < k && nums[k] === nums[--k]) {}
+                } else if (add < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+    }
+    return res;
+};
+//leetcode submit region end(Prohibit modification and deletion)
