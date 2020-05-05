ab1ec5fb38d40a6c2d93cdbb5d2d5714c9f7dd30
songzxDev <songzx_2326@163.com>
2020-05-05 10:24:47 +0800

    //leetcode submit region begin(Prohibit modification and deletion) /**  * leetcode72.编辑距离 https://leetcode-cn.com/problems/edit-distance/submissions/  * @param {string} word1  * @param {string} word2  * @return {number}  */ const minDistance = function (word1, word2) {     if (word1 === word2) return 0;     let n1 = word1.length, n2 = word2.length, dp = [Array.from({length: n2 + 1}, (_, i) => i)]; // 第一行     for (let i = 1; i < n1 + 1; i++) {         dp.push(new Array(n2 + 1).fill(0));         dp[i][0] = dp[i - 1][0] + 1;// 第一列     }     for (let i = 1; i < n1 + 1; i++) {         for (let j = 1; j < n2 + 1; j++) {             if (word1.charAt(i - 1) === word2.charAt(j - 1)) {                 dp[i][j] = dp[i - 1][j - 1];             } else {                 dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;             }         }     }     return dp[n1][n2]; }; //leetcode submit region end(Prohibit modification and deletion)

--- leatcode/lib/2020/test02.js
+++ leatcode/lib/2020/test02.js
@@ -187,3 +187,65 @@ const groupAnagrams = function (strs) {
     });
     return [...countMap.values()];
 };
+
+
+//给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+//
+// 你可以对一个单词进行如下三种操作：
+//
+//
+// 插入一个字符
+// 删除一个字符
+// 替换一个字符
+//
+//
+//
+//
+// 示例 1：
+//
+// 输入：word1 = "horse", word2 = "ros"
+//输出：3
+//解释：
+//horse -> rorse (将 'h' 替换为 'r')
+//rorse -> rose (删除 'r')
+//rose -> ros (删除 'e')
+//
+//
+// 示例 2：
+//
+// 输入：word1 = "intention", word2 = "execution"
+//输出：5
+//解释：
+//intention -> inention (删除 't')
+//inention -> enention (将 'i' 替换为 'e')
+//enention -> exention (将 'n' 替换为 'x')
+//exention -> exection (将 'n' 替换为 'c')
+//exection -> execution (插入 'u')
+//
+// Related Topics 字符串 动态规划
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * leetcode72.编辑距离 https://leetcode-cn.com/problems/edit-distance/submissions/
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+const minDistance = function (word1, word2) {
+    if (word1 === word2) return 0;
+    let n1 = word1.length, n2 = word2.length, dp = [Array.from({length: n2 + 1}, (_, i) => i)]; // 第一行
+    for (let i = 1; i < n1 + 1; i++) {
+        dp.push(new Array(n2 + 1).fill(0));
+        dp[i][0] = dp[i - 1][0] + 1;// 第一列
+    }
+    for (let i = 1; i < n1 + 1; i++) {
+        for (let j = 1; j < n2 + 1; j++) {
+            if (word1.charAt(i - 1) === word2.charAt(j - 1)) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;
+            }
+        }
+    }
+    return dp[n1][n2];
+};
+//leetcode submit region end(Prohibit modification and deletion)
