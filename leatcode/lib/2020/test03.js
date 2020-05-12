91a296d48485e7187ab2f18902ff583c04250a99
songzxDev <songzx_2326@163.com>
2020-05-12 08:53:28 +0800

    提交将getLinkNodeFromArray，数组转换为链表的函数

--- leatcode/lib/2020/test03.js
+++ leatcode/lib/2020/test03.js
@@ -1,7 +1,23 @@
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
+/**
+ * 将数组转换为一个链表
+ * @param {Array} array
+ * @returns {*}
+ */
+const getLinkNodeFromArray = (array) => {
+    function ListNode(val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    let node = null;
+    if (Array.isArray(array) && array.length > 0) {
+        node = new ListNode(array[0]);
+        for (let i = 1; i < array.length; i++) {
+            eval(`node.${new Array(i).fill('next').join('.')} = new ListNode(${array[i]})`);
+        }
+    }
+    return node;
+};
 
 /**
  * 题目21.合并两个有序链表 https://leetcode-cn.com/problems/merge-two-sorted-lists/
@@ -21,15 +37,8 @@ const mergeTwoLists = function (l1, l2) {
     }
 };
 
-let a = [1, 2, 4, 7], b = [2, 5, 8, 11, 19];
+let a = [-9, -2, -1, 0, 1, 2, 4, 7], b = [2, 5, 8, 11, 19, 21];
 
-let node1 = new ListNode(a[0]), node2 = new ListNode(b[0]);
-for (let i = 1; i < a.length; i++) {
-    eval(`node1.${new Array(i).fill('next').join('.')} = new ListNode(${a[i]})`);
-}
-for (let j = 1; j < b.length; j++) {
-    eval(`node2.${new Array(j).fill('next').join('.')} = new ListNode(${b[j]})`);
-}
-
+let node1 = getLinkNodeFromArray(a), node2 = getLinkNodeFromArray(b);
 let afterMerge = mergeTwoLists(node1, node2);
 console.log(JSON.stringify(afterMerge));
