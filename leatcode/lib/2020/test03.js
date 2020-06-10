f278cdd6d6a94589c64c46fbde13488ea57581fe
songzxDev <songzx_2326@163.com>
2020-06-10 08:53:15 +0800

    偷房子

--- leatcode/lib/2020/test03.js
+++ leatcode/lib/2020/test03.js
@@ -235,7 +235,7 @@ const minWindow = function (s, t) {
  * @param {number[]} nums
  * @return {number}
  */
-const removeDuplicates = function(nums) {
+const removeDuplicates = function (nums) {
     let r = 0;
     for (let i = 0; i < nums.length; i++) {
         if (i === 0 || nums[i - 1] < nums[i]) {
@@ -270,7 +270,7 @@ const removeDuplicates = function(nums) {
  * @param {ListNode} l2
  * @return {ListNode}
  */
-const mergeTwoLists = function(l1, l2) {
+const mergeTwoLists = function (l1, l2) {
     if (!l1) return l2;
     if (!l2) return l1;
     if (l1.val < l2.val) {
@@ -313,9 +313,58 @@ const mergeTwoLists = function(l1, l2) {
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-const merge = function(nums1, m, nums2, n) {
-    let i = m - 1, j = n - 1, k = m + n -1;
+const merge = function (nums1, m, nums2, n) {
+    let i = m - 1, j = n - 1, k = m + n - 1;
     while (i >= 0 && j >= 0) nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
     while (j >= 0) nums1[j] = nums2[j--];
+};
+//leetcode submit region end(Prohibit modification and deletion)
+//你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上
+//被小偷闯入，系统会自动报警。
+//
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+//
+//
+//
+// 示例 1：
+//
+// 输入：[1,2,3,1]
+//输出：4
+//解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+//     偷窃到的最高金额 = 1 + 3 = 4 。
+//
+// 示例 2：
+//
+// 输入：[2,7,9,3,1]
+//输出：12
+//解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+//     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= nums.length <= 100
+// 0 <= nums[i] <= 400
+//
+// Related Topics 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const rob = function (nums) {
+    if (!nums || nums.length === 0) return 0;
+    const n = nums.length, stolen = [[0, nums[0]]];
+    for (let i = 1; i < n; i++) stolen.push([0, 0]);
+    for (let i = 1; i < n; i++) {
+        stolen[i][1] = nums[i] + stolen[i - 1][0];
+        stolen[i][0] = Math.max(stolen[i - 1][0], stolen[i - 1][1]);
+    }
+    return Math.max(stolen[n - 1][0], stolen[n - 1][1]);
 };
 //leetcode submit region end(Prohibit modification and deletion)
