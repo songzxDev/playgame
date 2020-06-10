b06954d0530de4951320c735703104de8a2c2af0
songzxDev <songzx_2326@163.com>
2020-06-10 08:27:05 +0800

    最小覆盖子串

--- leatcode/lib/2020/test03.js
+++ leatcode/lib/2020/test03.js
@@ -162,3 +162,44 @@ const groupAnagrams = function (strs) {
     return [...groupMap.values()];
 };
 //leetcode submit region end(Prohibit modification and deletion)
+//给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
+//
+// 示例：
+//
+// 输入: S = "ADOBECODEBANC", T = "ABC"
+//输出: "BANC"
+//
+// 说明：
+//
+//
+// 如果 S 中不存这样的子串，则返回空字符串 ""。
+// 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+//
+// Related Topics 哈希表 双指针 字符串 Sliding Window
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+const minWindow = function (s, t) {
+    if (!s || !t || s.length < t.length) return '';
+    let [i, j, start, minLen, found, sMap, tMap] = [0, 0, 0, 0x7fffffff, 0, [], []];
+    for (let i = 0; i < 256; i++) sMap.push(tMap.push(0) & 0);
+    for (let j = 0; j < t.length; j++) tMap[t.charCodeAt(j)]++;
+    while (j < s.length) {
+        if (found < t.length) {
+            const prev = s.charCodeAt(j++);
+            if (tMap[prev] > 0) if (++sMap[prev] <= tMap[prev]) found++;
+        }
+        while (found === t.length) {
+            if (j - i < minLen) [start, minLen] = [i, j - i];
+            const next = s.charCodeAt(i++);
+            if (tMap[next] > 0) if (--sMap[next] < tMap[next]) found--;
+        }
+    }
+    return minLen === 0x7fffffff ? "" : s.substr(start, minLen);
+};
+//leetcode submit region end(Prohibit modification and deletion)
