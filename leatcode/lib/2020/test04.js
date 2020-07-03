9706d3e65ffb6995b49b4255ba0a5d8bd826e389
songzxDev <songzx_2326@163.com>
2020-07-03 14:14:57 +0800

    归并 change

--- leatcode/lib/2020/test04.js
+++ leatcode/lib/2020/test04.js
@@ -20,21 +20,20 @@ class SortArrayUtil {
 
     static mergeSort(array, left, right) {
         if (left >= right) return;
-        const mid = (left + right) >> 1;
+        let mid = (left + right) >> 1;
         this.mergeSort(array, left, mid);
         this.mergeSort(array, mid + 1, right);
         this.merge(array, left, mid, right);
     }
 
     static merge(array, left, mid, right) {
-        const temp = new Array(right - left + 1).fill(0);
-        let i = left, j = mid + 1, k = 0;
+        let i = left, j = mid + 1, k = 0, tmp = [];
         while (i <= mid && j <= right) {
-            temp[k++] = array[i] <= array[j] ? array[i++] : array[j++];
+            tmp.push(array[i] < array[j] ? array[i++] : array[j++]);
         }
-        while (i <= mid) temp[k++] = array[i++];
-        while (j <= right) temp[k++] = array[j++];
-        for (let p = 0; p < temp.length; p++) array[left + p] = temp[p];
+        while (i <= mid) tmp.push(array[i++]);
+        while (j <= right) tmp.push(array[j++]);
+        for (let p = 0; p < tmp.length; p++) array[left + p] = tmp[p];
     }
 }
 
