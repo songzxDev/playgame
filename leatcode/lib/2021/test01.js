c83298968cfcd0afae586477ab7748bccd00706e
songzx_2326@163.com <huyu097308@163.com>
2021-03-03 18:44:46 +0800

    提交myprod文件

--- leatcode/lib/2021/test01.js
+++ leatcode/lib/2021/test01.js
@@ -4,24 +4,25 @@
  * @param {string[][]} grid
  * @return {number}
  */
-const numIslands = function(grid) {
-    let m = grid.length, n = grid[0].length, count = 0;
-    const dfsHelper = (grid, i, j) => {
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === '0') return;
-        grid[i][j] = '0';
-        dfsHelper(grid, i - 1, j);
-        dfsHelper(grid, i + 1, j);
-        dfsHelper(grid, i, j - 1);
-        dfsHelper(grid, i, j + 1);
-    };
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === '1') {
-                dfsHelper(grid, i, j);
-                count++;
-            }
-        }
-    }
-    return count;
+const numIslands = function (grid) {
+   let dx = [-1, 1, 0, 0], dy = [0, 0, -1, 1], m = grid.length, n = grid[0].length, count = 0;
+   const dfsFill = (i, j) => {
+       if (grid[i][j] === '0') return 0;
+       grid[i][j] = '0';
+       for (let k = 0; k < dx.length; k++) {
+           let x = i + dx[k], y = j + dy[k];
+           if (x < 0 || y < 0 || x >= m || y >= n) continue;
+           dfsFill(x, y);
+       }
+       return 1;
+   };
+   for (let i = 0; i < m; i++) {
+       for (let j = 0; j < n; j++) {
+           if (grid[i][j] === '1') {
+               count += dfsFill(i, j);
+           }
+       }
+   }
+   return count;
 };
 //leetcode submit region end(Prohibit modification and deletion)
